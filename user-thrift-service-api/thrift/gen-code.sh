96464655eb2945f3bf24fff00931777c477870e3
oracleOCM <baishufan@126.com>
2019-01-12 18:51:12 +0800

    second commit

---
+++ user-thrift-service-api/thrift/gen-code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+thrift --gen java -out ../src/main/java ./user_service.thrift
+
+
---
+++ user-thrift-service/src/main/java/com/imooc/user/ServiceApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by oracleOCM on 2019/1/12.
+ */
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main (String [] args){
+        SpringApplication.run(SpringApplication.class,args);
+    }
+}
