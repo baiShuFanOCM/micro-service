474ec1a0c471360c3d8888b89f7fcbba229986ca
oracleOCM <baishufan@126.com>
2019-01-13 17:14:16 +0800

    code update

---
+++ user-edge-service/src/main/java/com/imooc/user/ServiceApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by oracleOCM on 2019/1/13.
+ */
+
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main (String [] args){
+        SpringApplication.run(ServiceApplication.class,args);
+    }
+
+
+}
